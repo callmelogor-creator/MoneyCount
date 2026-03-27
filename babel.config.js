4416c2786caae20982b2f3a32f051f24b4c689ab
callmelogor-creator <callmelogor@gmail.com>
2026-03-27 14:34:00 +0800

    fix babel config

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin', // 淨係要呢個，其他全部唔要住
+      'react-native-reanimated/plugin', // 只留呢句就夠
     ],
   };
 };
